1f34fce6d808718a64d825bd135f31044cf52898
IceCheeseCat <icecheesecat@gmail.com>
2025-01-24 13:53:53 +0800

    deleted

---
+++ src/main/java/com/github/icecheesecat/kantaicraft/capability/InventoryProvider.java
@@ -0,0 +1,23 @@
+//package com.github.icecheesecat.kantaicraft.capability;
+//
+//import net.minecraft.core.Direction;
+//import net.minecraft.nbt.CompoundTag;
+//import net.minecraft.nbt.Tag;
+//import net.minecraftforge.common.capabilities.*;
+//import net.minecraftforge.common.util.LazyOptional;
+//import net.minecraftforge.items.IItemHandler;
+//import net.minecraftforge.items.ItemStackHandler;
+//import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.Nullable;
+//
+//public class InventoryProvider implements ICapabilitySerializable<CompoundTag> {
+//
+//
+//    final int size;
+//
+//    public InventoryProvider(int size) {
+//        this.size = size;
+//    }
+//
+//
+//}
