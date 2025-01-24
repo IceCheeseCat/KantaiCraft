894f297de4b28022f8a5e428cf8ba269684f74a7
IceCheeseCat <icecheesecat@gmail.com>
2025-01-24 15:24:22 +0800

    deleted

--- src/main/java/com/github/icecheesecat/shincolle/equipment/cannon/model/ShellModel.java
+++
@@ -1,9 +0,0 @@
-package com.github.icecheesecat.shincolle.equipment.cannon.model;
-
-import net.minecraftforge.client.extensions.IForgeBakedModel;
-
-public class ShellModel implements IForgeBakedModel {
-
-
-
-}
--- src/main/java/com/github/icecheesecat/shincolle/util/goal/GoalStates.java
+++
@@ -1,8 +0,0 @@
-package com.github.icecheesecat.shincolle.util.goal;
-
-public enum GoalStates {
-
-    RUNNING,
-    LOSS_TARGET_SIGHT
-
-}
