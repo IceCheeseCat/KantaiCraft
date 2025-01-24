a3aac175c5664a5d9a469f9fa5f410adb02dded7
IceCheeseCat <icecheesecat@gmail.com>
2025-01-24 14:01:39 +0800

    Context string changed

--- src/main/java/com/github/icecheesecat/kantaicraft/registries/ModShipAttributes.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/registries/ModShipAttributes.java
@@ -1,7 +1,7 @@
 package com.github.icecheesecat.kantaicraft.registries;
 
 import com.github.icecheesecat.kantaicraft.KantaiCraft;
-import com.github.icecheesecat.kantaicraft.stats.shipAttributes.ShipAttribute;
+import com.github.icecheesecat.kantaicraft.stats.shipAttributes.SyncableAttribute;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,17 +11,17 @@ public class ModShipAttributes {
 
     public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, KantaiCraft.MODID);
 
-    public static final RegistryObject<Attribute> FIREPOWER = ATTRIBUTES.register("shipstats.firepower", () -> new ShipAttribute("shipstats.firepower", 10.0d));
-    public static final RegistryObject<Attribute> TORPEDO = ATTRIBUTES.register("shipstats.torpedo", () -> new ShipAttribute("shipstats.torpedo", 5.0d));
-    public static final RegistryObject<Attribute> ANTIAIR = ATTRIBUTES.register("shipstats.antiair", () -> new ShipAttribute("shipstats.antiair", 20.0d));
-    public static final RegistryObject<Attribute> ASW = ATTRIBUTES.register("shipstats.asw", () -> new ShipAttribute("shipstats.asw", 30.0d));
-    public static final RegistryObject<Attribute> LOS = ATTRIBUTES.register("shipstats.los", () -> new ShipAttribute("shipstats.los", 30.0d));
-    public static final RegistryObject<Attribute> LUCK = ATTRIBUTES.register("shipstats.luck", () -> new ShipAttribute("shipstats.luck", 10.0d));
-    public static final RegistryObject<Attribute> ARMOR = ATTRIBUTES.register("shipstats.armor", () -> new ShipAttribute("shipstats.armor", 50.0d));
-    public static final RegistryObject<Attribute> EVASION = ATTRIBUTES.register("shipstats.evasion", () -> new ShipAttribute("shipstats.evasion", 10.0d));
-    public static final RegistryObject<Attribute> AIRCRAFT = ATTRIBUTES.register("shipstats.aircraft", () -> new ShipAttribute("shipstats.aircraft", 0.0d));
-    public static final RegistryObject<Attribute> FUEL = ATTRIBUTES.register("shipstats.fuel", () -> new ShipAttribute("shipstats.fuel", 10.0d));
-    public static final RegistryObject<Attribute> AMMO = ATTRIBUTES.register("shipstats.ammo", () -> new ShipAttribute("shipstats.ammo", 12.0d));
-    public static final RegistryObject<Attribute> SLOT_SIZE = ATTRIBUTES.register("shipstats.slotsize", () -> new ShipAttribute("shipstats.slotsize", 4.0d));
+    public static final RegistryObject<Attribute> FIREPOWER = ATTRIBUTES.register("firepower", () -> new SyncableAttribute("firepower", 10.0d));
+    public static final RegistryObject<Attribute> TORPEDO = ATTRIBUTES.register("torpedo", () -> new SyncableAttribute("torpedo", 5.0d));
+    public static final RegistryObject<Attribute> ANTIAIR = ATTRIBUTES.register("antiair", () -> new SyncableAttribute("antiair", 20.0d));
+    public static final RegistryObject<Attribute> ASW = ATTRIBUTES.register("asw", () -> new SyncableAttribute("asw", 30.0d));
+    public static final RegistryObject<Attribute> LOS = ATTRIBUTES.register("los", () -> new SyncableAttribute("los", 30.0d));
+    public static final RegistryObject<Attribute> LUCK = ATTRIBUTES.register("luck", () -> new SyncableAttribute("luck", 10.0d));
+    public static final RegistryObject<Attribute> ARMOR = ATTRIBUTES.register("armor", () -> new SyncableAttribute("armor", 50.0d));
+    public static final RegistryObject<Attribute> EVASION = ATTRIBUTES.register("evasion", () -> new SyncableAttribute("evasion", 10.0d));
+    public static final RegistryObject<Attribute> AIRCRAFT = ATTRIBUTES.register("aircraft", () -> new SyncableAttribute("aircraft", 0.0d));
+    public static final RegistryObject<Attribute> FUEL = ATTRIBUTES.register("fuel", () -> new SyncableAttribute("fuel", 10.0d));
+    public static final RegistryObject<Attribute> AMMO = ATTRIBUTES.register("ammo", () -> new SyncableAttribute("ammo", 12.0d));
+    public static final RegistryObject<Attribute> SLOT_SIZE = ATTRIBUTES.register("slotsize", () -> new SyncableAttribute("slotsize", 4.0d));
 
 }
