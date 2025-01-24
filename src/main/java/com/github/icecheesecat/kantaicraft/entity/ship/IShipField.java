f14b971f1cb3f70af541af8231392de731b1759d
IceCheeseCat <icecheesecat@gmail.com>
2025-01-24 13:54:12 +0800

    changed

--- src/main/java/com/github/icecheesecat/kantaicraft/entity/ship/IShipField.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/entity/ship/IShipField.java
@@ -2,8 +2,9 @@ package com.github.icecheesecat.kantaicraft.entity.ship;
 
 import com.github.icecheesecat.kantaicraft.util.ShipFields;
 
-public interface IShipClass {
+public interface IShipField {
 
     ShipFields.ShipClass getShipClass();
+    ShipFields.ShipName getShipName();
 
 }
