cb89be58b0f76502fffb1ae3c4d92846298385f4
ice20 <ice2002.5@gmail.com>
2025-02-11 23:20:18 +0800

    Fixed PickUpKilledMobDrops not smooth

--- src/main/java/com/github/icecheesecat/kantaicraft/brain/ship/behavior/PickUpKilledMobDrops.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/brain/ship/behavior/PickUpKilledMobDrops.java
@@ -1,17 +1,18 @@
-package com.github.icecheesecat.kantaicraft.brain.ship;
+package com.github.icecheesecat.kantaicraft.brain.ship.behavior;
 
 import com.github.icecheesecat.kantaicraft.entity.ship.BasicEntityShip;
 import com.github.icecheesecat.kantaicraft.registries.ModMemoryModuleType;
 import com.google.common.collect.ImmutableMap;
-import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.memory.WalkTarget;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.pathfinder.Path;
 import net.minecraftforge.items.IItemHandler;
 
 import java.util.ArrayList;
@@ -23,6 +24,9 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
 
     private ItemEntity itemEntity;
     private static final int TIMEOUT = 200;
+    private int hasAttackTargetCountdown;
+    private static final int MAX_HAS_ATTACK_TARGET_COUNTDOWN = 5 * 20;
+    private WalkTarget walkTarget;
     private boolean stopped;
 
     public PickUpKilledMobDrops() {
@@ -30,16 +34,17 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
                 ImmutableMap.of(ModMemoryModuleType.KILLED_ENTITY_DROPS.get(), MemoryStatus.VALUE_PRESENT,
                         MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE, MemoryStatus.REGISTERED,
                         MemoryModuleType.WALK_TARGET, MemoryStatus.REGISTERED,
-                        MemoryModuleType.PATH, MemoryStatus.REGISTERED,
-                        MemoryModuleType.ATTACK_TARGET, MemoryStatus.VALUE_ABSENT),
+                        MemoryModuleType.PATH, MemoryStatus.REGISTERED),
                 TIMEOUT);
+        this.hasAttackTargetCountdown = MAX_HAS_ATTACK_TARGET_COUNTDOWN;
     }
 
     @Override
     protected void start(ServerLevel pLevel, BasicEntityShip pEntity, long pGameTime) {
 //        this.tickReCalcPath = 0;
         this.stopped = false;
-        pEntity.getBrain().setMemory(MemoryModuleType.WALK_TARGET, new WalkTarget(itemEntity, 1.0f, 0));
+        this.walkTarget = new WalkTarget(itemEntity.blockPosition(), 1.0f, 0);
+        pEntity.getBrain().setMemory(MemoryModuleType.WALK_TARGET, this.walkTarget);
     }
 
     @Override
@@ -49,7 +54,12 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
             return;
         }
 
-        if (pOwner.distanceTo(itemEntity) < 1.4d) {
+        if (pOwner.blockPosition().equals(this.walkTarget.getTarget().currentBlockPosition())) {
+            System.out.println("At Spot!!!");
+        }
+
+        double dis = pOwner.distanceTo(itemEntity);
+        if (dis < 1.5d) {
             // memory remove same itemEntity
             pOwner.getBrain().getMemory(ModMemoryModuleType.KILLED_ENTITY_DROPS.get()).ifPresent(
                     itemEntities -> {
@@ -63,6 +73,18 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
             this.stopped = true;
         }
 
+        Path path;
+        if (pOwner.getBrain().getMemory(MemoryModuleType.PATH).isPresent()) {
+            path = pOwner.getBrain().getMemory(MemoryModuleType.PATH).get();
+            System.out.println(path);
+            for (var node: path.getClosedSet()) {
+                System.out.println(" " + node.asBlockPos());
+            }
+        }
+
+
+
+
     }
 
     @Override
@@ -73,20 +95,51 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
 
     @Override
     protected boolean canStillUse(ServerLevel pLevel, BasicEntityShip pEntity, long pGameTime) {
-        return !this.stopped && !pEntity.getBrain().hasMemoryValue(MemoryModuleType.ATTACK_TARGET);
+        if (pEntity.getBrain().hasMemoryValue(MemoryModuleType.ATTACK_TARGET)) {
+            this.hasAttackTargetCountdown = MAX_HAS_ATTACK_TARGET_COUNTDOWN;
+            return false;
+        }
+        if (changedWalkTarget(pEntity)) {
+            return false;
+        }
+        return !this.stopped && this.itemEntity != null && !this.itemEntity.isRemoved();
     }
 
+    @Override
+    protected boolean timedOut(long pGameTime) {
+        return false;
+    }
+
+    private boolean changedWalkTarget(LivingEntity entity) {
+        if (entity.getBrain().getMemory(MemoryModuleType.WALK_TARGET).isPresent()) {
+            var memWalkTarget = entity.getBrain().getMemory(MemoryModuleType.WALK_TARGET).get();
+            if (memWalkTarget.equals(this.walkTarget)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     /**
      * Check whether there is room for a {@link net.minecraft.world.item.ItemStack} to insert into the ship inventory
      */
     @Override
     protected boolean checkExtraStartConditions(ServerLevel pLevel, BasicEntityShip pOwner) {
+        if (pOwner.getBrain().hasMemoryValue(MemoryModuleType.ATTACK_TARGET)) {
+            this.hasAttackTargetCountdown = MAX_HAS_ATTACK_TARGET_COUNTDOWN;
+            return false;
+        }
+        if (this.hasAttackTargetCountdown > 0) {
+            this.hasAttackTargetCountdown--;
+            return false;
+        }
+
         var optional = pOwner.getBrain().getMemory(ModMemoryModuleType.KILLED_ENTITY_DROPS.get());
         if (optional.isPresent()) {
             var list = optional.get();
 
             if (list.isEmpty()) return false;
-            this.eraseRemovedItem(list); // handle removed itemEntity
+            this.eraseRemovedItemThanSort(list, pOwner); // handle removed itemEntity
 
             for (var ele: list) {
                 if (canInsetToInventory(ele, pOwner) && ele.onGround()) {
@@ -121,7 +174,7 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
         return false;
     }
 
-    private void eraseRemovedItem(List<ItemEntity> itemEntities) {
+    private void eraseRemovedItemThanSort(List<ItemEntity> itemEntities, Entity owner) {
 
         List<ItemEntity> removed = new ArrayList<>();
         for (var itemEntity: itemEntities) {
@@ -137,6 +190,11 @@ public class PickUpKilledMobDrops extends Behavior<BasicEntityShip> {
         }
 
         itemEntities.removeAll(removed);
+        itemEntities.sort((ie1, ie2) -> {
+            if (ie1.distanceTo(owner) < ie2.distanceTo(owner)) return -1;
+            else if (ie1.distanceTo(owner) > ie2.distanceTo(owner)) return 1;
+            return 0;
+        });
 
     }
 }
