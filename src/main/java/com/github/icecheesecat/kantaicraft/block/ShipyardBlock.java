418ae737d5b8a2c1f742bc2502f40561708e4056
IceCheeseCat <icecheesecat@gmail.com>
2025-02-18 10:57:12 +0800

    drop content in ShipyardBlock

--- src/main/java/com/github/icecheesecat/kantaicraft/block/ShipyardBlock.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/block/ShipyardBlock.java
@@ -1,12 +1,16 @@
 package com.github.icecheesecat.kantaicraft.block;
 
+import com.github.icecheesecat.kantaicraft.block.shipyardUtil.BuiltData;
+import com.github.icecheesecat.kantaicraft.item.ShipBlueprintData;
 import com.github.icecheesecat.kantaicraft.registries.ModBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -14,8 +18,11 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ShipyardBlock extends BaseEntityBlock {
 
@@ -46,5 +53,37 @@ public class ShipyardBlock extends BaseEntityBlock {
         }
 
         return InteractionResult.FAIL;
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        if (pLevel.getBlockEntity(pPos) instanceof ShipyardBlockEntity shipyardBlockEntity) {
+            double x = pPos.getCenter().x;
+            double y = pPos.getCenter().y;
+            double z = pPos.getCenter().z;
+
+            // drop itemHandler
+            shipyardBlockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(
+                    itemHandler -> {
+
+                        for (int i = 0; i < itemHandler.getSlots(); i++) {
+                            ItemStack itemStack = itemHandler.getStackInSlot(i);
+                            if (itemStack.isEmpty()) continue;
+                            ItemEntity itemEntity = new ItemEntity(pLevel, x, y, z, itemStack);
+                            pLevel.addFreshEntity(itemEntity);
+                        }
+                    }
+            );
+
+            // drop built data
+            List<BuiltData> builtData = shipyardBlockEntity.getBuiltData();
+            for (var data: builtData) {
+                ItemStack itemStack = data.getData().createItemStack();
+                ItemEntity itemEntity = new ItemEntity(pLevel, x, y, z, itemStack);
+                pLevel.addFreshEntity(itemEntity);
+            }
+        }
+
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
     }
 }
