ee56c3e3e26d755c8f557953dc97b4ea51391a45
ice20 <ice2002.5@gmail.com>
2025-10-01 14:12:27 +0800

    EntityShip head rotations

--- src/main/java/com/github/icecheesecat/kantaicraft/client/model/model/InazumaModel.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/client/model/model/InazumaModel.java
@@ -3,21 +3,26 @@ package com.github.icecheesecat.kantaicraft.client.model.model;
 import com.github.icecheesecat.kantaicraft.KantaiCraft;
 import com.github.icecheesecat.kantaicraft.entity.ship.destroyer.Inazuma;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 
-public class InazumaModel<T extends Inazuma> extends GeoModel<T> {
-    @Override
-    public ResourceLocation getModelResource(Inazuma inazuma) {
-        return new ResourceLocation(KantaiCraft.MODID, "geo/inazuma.geo.json");
+public class InazumaModel<T extends Inazuma> extends DefaultedEntityGeoModel<T> {
+    public InazumaModel() {
+        super(new ResourceLocation(KantaiCraft.MODID, "inazuma"), true);
     }
 
-    @Override
-    public ResourceLocation getTextureResource(Inazuma inazuma) {
-        return new ResourceLocation(KantaiCraft.MODID, "textures/entity/inazuma.png");
-    }
-
-    @Override
-    public ResourceLocation getAnimationResource(Inazuma inazuma) {
-        return new ResourceLocation(KantaiCraft.MODID, "animations/inazuma.animation.json");
-    }
+//    @Override
+//    public ResourceLocation getModelResource(Inazuma inazuma) {
+//        return new ResourceLocation(KantaiCraft.MODID, "geo/inazuma.geo.json");
+//    }
+//
+//    @Override
+//    public ResourceLocation getTextureResource(Inazuma inazuma) {
+//        return new ResourceLocation(KantaiCraft.MODID, "textures/entity/inazuma.png");
+//    }
+//
+//    @Override
+//    public ResourceLocation getAnimationResource(Inazuma inazuma) {
+//        return new ResourceLocation(KantaiCraft.MODID, "animations/inazuma.animation.json");
+//    }
 }
