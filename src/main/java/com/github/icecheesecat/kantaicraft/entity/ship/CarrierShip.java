ffc59baffa86901a3a3dffdcc422f024af6fa367
ice20 <ice2002.5@gmail.com>
2025-10-23 12:07:01 +0800

    Hid CarrierShip.java related

--- src/main/java/com/github/icecheesecat/kantaicraft/entity/ship/CarrierShip.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/entity/ship/CarrierShip.java
@@ -1,31 +1,32 @@
-package com.github.icecheesecat.kantaicraft.entity.ship;
-
-import com.github.icecheesecat.kantaicraft.entity.brain.ship.CarrierBrain;
-import com.github.icecheesecat.kantaicraft.equipment.EquipmentType;
-import com.google.common.collect.ImmutableList;
-import com.mojang.serialization.Dynamic;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.ai.Brain;
-import net.minecraft.world.level.Level;
-
-import java.util.List;
-
-public abstract class CarrierShip extends EntityShip {
-
-    public static final List<EquipmentType> ATTACKABLE_TYPES = ImmutableList.of(EquipmentType.AIRCRAFT_FIGHTER);
-
-    protected CarrierShip(EntityType<? extends PathfinderMob> entityType, Level level) {
-        super(entityType, ShipClass.AIRCRAFT_CARRIER, level, ATTACKABLE_TYPES);
-    }
-
-    @Override
-    public Brain<EntityShip> getBrain() {
-        return (Brain<EntityShip>) this.brain;
-    }
-
-    @Override
-    protected Brain<?> makeBrain(Dynamic<?> dyn) {
-        return CarrierBrain.makeBrain(this, dyn);
-    }
-}
+//package com.github.icecheesecat.kantaicraft.entity.ship;
+//
+//import com.github.icecheesecat.kantaicraft.entity.brain.ship.CarrierBrain;
+//import com.github.icecheesecat.kantaicraft.entity.stance.EntityStance;
+//import com.github.icecheesecat.kantaicraft.equipment.EquipmentType;
+//import com.google.common.collect.ImmutableList;
+//import com.mojang.serialization.Dynamic;
+//import net.minecraft.world.entity.EntityType;
+//import net.minecraft.world.entity.PathfinderMob;
+//import net.minecraft.world.entity.ai.Brain;
+//import net.minecraft.world.level.Level;
+//
+//import java.util.List;
+//
+//public abstract class CarrierShip extends EntityShip {
+//
+//    public static final List<EquipmentType> ATTACKABLE_TYPES = ImmutableList.of(EquipmentType.AIRCRAFT_FIGHTER);
+//
+//    protected CarrierShip(EntityType<? extends PathfinderMob> entityType, Level level, EntityStance entityStance) {
+//        super(entityType, ShipClass.AIRCRAFT_CARRIER, level, ATTACKABLE_TYPES, entityStance);
+//    }
+//
+//    @Override
+//    public Brain<EntityShip> getBrain() {
+//        return (Brain<EntityShip>) this.brain;
+//    }
+//
+//    @Override
+//    protected Brain<?> makeBrain(Dynamic<?> dyn) {
+//        return CarrierBrain.makeBrain(this, dyn);
+//    }
+//}
