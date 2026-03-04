fed7585ae1268d5d5babeafb6e404cad403c39f8
ice20 <ice2002.5@gmail.com>
2026-03-04 21:46:03 +0800

    AxisRotation util class

--- src/main/java/com/github/icecheesecat/kantaicraft/model/EntityShipRenderer.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/model/EntityShipRenderer.java
@@ -6,6 +6,7 @@ import com.github.icecheesecat.kantaicraft.equipment.EquipmentManager;
 import com.github.icecheesecat.kantaicraft.equipment.handler.EquipmentHandler;
 import com.github.icecheesecat.kantaicraft.model.equipment.EquippableDetailSlots;
 import com.github.icecheesecat.kantaicraft.model.equipment.renderer.EquipmentRenderer;
+import com.github.icecheesecat.kantaicraft.util.AxisRotation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
@@ -203,16 +204,8 @@ public abstract class EntityShipRenderer<T extends EntityShip> extends GeoEntity
             float deg = 360.0f/ray_count * i;
 
             Vec3 A = viewVector.cross(new Vec3(0,1,0));
-            var q = Axis.of(A.toVector3f()).rotationDegrees(5.0f);
-            var iq = q.invert(new Quaternionf());
-            q.mul((float) viewVector.x, (float) viewVector.y, (float) viewVector.z, 0).mul(iq);
-            Vec3 lifted = new Vec3(q.x, q.y, q.z);
-
-            var q2 = Axis.of(viewVector.toVector3f()).rotationDegrees(deg);
-            var iq2 = q2.invert(new Quaternionf());
-            q2.mul((float) lifted.x, (float) lifted.y, (float) lifted.z, 0).mul(iq2);
-            Vec3 rotated = new Vec3(q2.x, q2.y, q2.z);
-
+            Vec3 lifted = AxisRotation.getRotatedVectorAroundAnAxis(Axis.of(A.toVector3f()), viewVector, 5.0f);
+            Vec3 rotated = AxisRotation.getRotatedVectorAroundAnAxis(Axis.of(viewVector.toVector3f()), lifted, deg);
             Vec3 rotated_end = eyeStart.add(rotated.multiply(10, 10, 10));
             vertexConsumer.vertex(matrix4f, (float) eyeStart.x, (float) eyeStart.y, (float) eyeStart.z).color(f4, f5, f6, f7).endVertex();
             vertexConsumer.vertex(matrix4f, (float) rotated_end.x, (float) rotated_end.y, (float) rotated_end.z).color(f4, f5, f6, f7).endVertex();
