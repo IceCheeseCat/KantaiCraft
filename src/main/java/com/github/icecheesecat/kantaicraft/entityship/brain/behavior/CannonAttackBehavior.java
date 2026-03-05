7207bc6c8176247ec40ea21351ef43c3add49880
ice20 <ice2002.5@gmail.com>
2026-03-05 14:28:33 +0800

    Added condition when attack target is in sight, then shall it use range attack

--- src/main/java/com/github/icecheesecat/kantaicraft/entityship/brain/behavior/CannonAttackBehavior.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/entityship/brain/behavior/CannonAttackBehavior.java
@@ -20,7 +20,8 @@ public class CannonAttackBehavior extends Behavior<CannonEntityShip> {
     public CannonAttackBehavior() {
         super(ImmutableMap.of(
                 MemoryModuleType.ATTACK_TARGET, MemoryStatus.VALUE_PRESENT,
-                ModMemoryModuleType.ACTION_HANDLER.get(), MemoryStatus.VALUE_PRESENT));
+                ModMemoryModuleType.ACTION_HANDLER.get(), MemoryStatus.VALUE_PRESENT,
+                ModMemoryModuleType.ATTACK_TARGET_IN_SIGHT.get(), MemoryStatus.VALUE_PRESENT));
     }
 
     @Override
