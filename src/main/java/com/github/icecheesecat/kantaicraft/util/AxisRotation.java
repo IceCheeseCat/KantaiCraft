81dfb53b4a7ab13045c6e4ac378a971ff5d2d9df
ice20 <ice2002.5@gmail.com>
2026-03-05 14:40:56 +0800

    new method added

--- src/main/java/com/github/icecheesecat/kantaicraft/util/AxisRotation.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/util/AxisRotation.java
@@ -6,6 +6,13 @@ import org.joml.Quaternionf;
 
 public class AxisRotation {
 
+    public static final Vec3 XP = new Vec3(1,0,0); // EAST
+    public static final Vec3 XN = new Vec3(-1,0,0); // WEST
+    public static final Vec3 YP = new Vec3(0,1,0); // UP
+    public static final Vec3 YN = new Vec3(0,-1,0); // DOWN
+    public static final Vec3 ZP = new Vec3(0,0,1); // SOUTH
+    public static final Vec3 ZN = new Vec3(0,0,-1); // NORTH
+
     public static Vec3 getRotatedVectorAroundAnAxis(Axis axis, Vec3 toRotate, float degree) {
 
         Quaternionf q = axis.rotationDegrees(degree);
@@ -13,6 +20,12 @@ public class AxisRotation {
         q.mul((float) toRotate.x, (float) toRotate.y, (float) toRotate.z, 0).mul(iq);
 
         return new Vec3(q.x, q.y, q.z);
+
+    }
+
+    public static Vec3 getRotatedVectorAroundAnAxis(Vec3 axisVector, Vec3 toRotate, float degree) {
+
+        return getRotatedVectorAroundAnAxis(Axis.of(axisVector.toVector3f()), toRotate, degree);
 
     }
 
