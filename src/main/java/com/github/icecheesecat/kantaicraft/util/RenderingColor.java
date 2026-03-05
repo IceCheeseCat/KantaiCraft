2f8e33777536b64273633143b84b2bb45a26477a
ice20 <ice2002.5@gmail.com>
2026-03-05 21:32:24 +0800

    RenderingColor class created

---
+++ src/main/java/com/github/icecheesecat/kantaicraft/util/RenderingColor.java
@@ -0,0 +1,7 @@
+package com.github.icecheesecat.kantaicraft.util;
+
+import net.minecraft.util.FastColor;
+
+public class RenderingColor {
+    public static final int WHITE = FastColor.ARGB32.color(255, 255, 255, 255);
+}
