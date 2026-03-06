cf0b69dad346d5c56236dd3fae5afb9151c898bc
ice20 <ice2002.5@gmail.com>
2026-03-06 21:02:55 +0800

    Indicates item drops by EntityShip

--- src/main/java/com/github/icecheesecat/kantaicraft/event/ClientRenderEvent.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/event/ClientRenderEvent.java
@@ -1,14 +1,19 @@
 package com.github.icecheesecat.kantaicraft.event;
 
 import com.github.icecheesecat.kantaicraft.KantaiCraft;
-import com.github.icecheesecat.kantaicraft.capability.ClientLevelTrajectoryCapability;
+import com.github.icecheesecat.kantaicraft.capability.indicateditementities.IndicatedItemEntitiesCapability;
+import com.github.icecheesecat.kantaicraft.capability.trajectory.ClientLevelTrajectoryCapability;
+import com.github.icecheesecat.kantaicraft.client.renderer.IndicatedItemRenderer;
 import com.github.icecheesecat.kantaicraft.client.renderer.TrajectoryRenderer;
 import com.github.icecheesecat.kantaicraft.model.EntityShipRenderer;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -47,6 +52,28 @@ public class ClientRenderEvent {
 
         }
 
+    }
+
+    @SubscribeEvent
+    public static void renderIndicatedItemEntities(RenderLevelStageEvent event) {
+        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_SOLID_BLOCKS) return;
+        if (Minecraft.getInstance().level != null) {
+            ClientLevel level = Minecraft.getInstance().level;
+            PoseStack poseStack = event.getPoseStack();
+            MultiBufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+
+            level.getCapability(IndicatedItemEntitiesCapability.TOKEN).ifPresent(
+                    indicatedItemEntities -> {
+                        indicatedItemEntities.getItemEntities().forEach(id -> {
+                            Entity entity = Minecraft.getInstance().level.getEntity(id);
+                            if (entity instanceof ItemEntity itemEntity) {
+                                IndicatedItemRenderer.render(itemEntity, poseStack, bufferSource);
+                            }
+                        });
+                    }
+            );
+
+        }
     }
 
 
