04432c491406a101b7ec05b3247d8e0d2ca7903f
ice20 <ice2002.5@gmail.com>
2026-03-25 10:46:48 +0800

    CommandCenter dispatch in dock ship

--- src/main/java/com/github/icecheesecat/kantaicraft/menu/commandcenter/CommandCenterMenu.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/menu/commandcenter/CommandCenterMenu.java
@@ -2,6 +2,7 @@ package com.github.icecheesecat.kantaicraft.menu.commandcenter;
 
 import com.github.icecheesecat.kantaicraft.registries.ModBlock;
 import com.github.icecheesecat.kantaicraft.registries.ModMenu;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -12,15 +13,17 @@ import net.minecraft.world.item.ItemStack;
 public class CommandCenterMenu extends AbstractContainerMenu {
     final Player player;
     ContainerLevelAccess access;
+    BlockPos dispatchLocation;
 
-    public CommandCenterMenu(int pContainerId, Inventory inventory, ContainerLevelAccess access) {
+    public CommandCenterMenu(int pContainerId, Inventory inventory, ContainerLevelAccess access, BlockPos dispatchLocation) {
         super(ModMenu.COMMAND_CENTER_MENU.get(), pContainerId);
         this.player = inventory.player;
         this.access = access;
+        this.dispatchLocation = dispatchLocation;
     }
 
     public CommandCenterMenu(int containerId, Inventory playerInv, FriendlyByteBuf extraData) {
-        this(containerId, playerInv, ContainerLevelAccess.NULL);
+        this(containerId, playerInv, ContainerLevelAccess.NULL, extraData.readBlockPos());
 //        ModPacketHandler.INSTANCE.sendToServer(new PlayerKantaiDataPacket.Request(this.player.getId()));
     }
 
