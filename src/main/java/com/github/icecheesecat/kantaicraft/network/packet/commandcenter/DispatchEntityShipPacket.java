fab004bfe13346dd017f82a8834149fc41c5ef8f
ice20 <ice2002.5@gmail.com>
2026-03-25 10:48:09 +0800

    CommandCenter dispatch in dock ship

--- src/main/java/com/github/icecheesecat/kantaicraft/network/packet/commandcenter/DispatchEntityShipPacket.java
+++ src/main/java/com/github/icecheesecat/kantaicraft/network/packet/commandcenter/DispatchEntityShipPacket.java
@@ -14,27 +14,27 @@ import java.util.function.Supplier;
 /**
  * Summon the target entity from command center request
  */
-public class CommandCenterRequestSummonPacket {
+public class DispatchEntityShipPacket {
 
     UUID summonUUID;
     BlockPos summonLocation;
 
-    public CommandCenterRequestSummonPacket(UUID summonUUID, BlockPos summonLocation) {
+    public DispatchEntityShipPacket(UUID summonUUID, BlockPos summonLocation) {
         this.summonUUID = summonUUID;
         this.summonLocation = summonLocation;
     }
 
-    public static void encode(CommandCenterRequestSummonPacket packet, FriendlyByteBuf buf) {
+    public static void encode(DispatchEntityShipPacket packet, FriendlyByteBuf buf) {
         buf.writeUUID(packet.summonUUID);
         buf.writeBlockPos(packet.summonLocation);
     }
 
-    public static CommandCenterRequestSummonPacket decode(FriendlyByteBuf buf) {
-        return new CommandCenterRequestSummonPacket(buf.readUUID(), buf.readBlockPos());
+    public static DispatchEntityShipPacket decode(FriendlyByteBuf buf) {
+        return new DispatchEntityShipPacket(buf.readUUID(), buf.readBlockPos());
     }
 
     // handle summon to server level
-    public static void handle(CommandCenterRequestSummonPacket packet, Supplier<NetworkEvent.Context> ctx) {
+    public static void handle(DispatchEntityShipPacket packet, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
 
             ServerPlayer serverPlayer = ctx.get().getSender();
@@ -57,6 +57,7 @@ public class CommandCenterRequestSummonPacket {
         EntityShip entityShip = (EntityShip) ses.getEntityType().create(player.level());
         if (entityShip == null) return;
         entityShip.load(ses.getEntityTag());
+        entityShip.setShipOwner(player.getUUID());
         entityShip.setPos(summonLocation.getX() + 0.5f, summonLocation.getY(), summonLocation.getZ() + 0.5f);
         player.level().addFreshEntity(entityShip);
     }
